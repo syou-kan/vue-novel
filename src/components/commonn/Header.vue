f42c249afc49b5fd56beb9d27ed826e3c00251f7
syoukan <syoukan@16.com>
2025-02-27 21:23:15 +0800

    初始化

---
+++ src/components/commonn/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="header">
+    <Top/>
+    <!-- <NavBar/> -->
+  </div>
+</template>
+<script setup name="Header">
+import Top from '@/components/commonn/Top.vue'
+// import NavBar from '@/components/commonn/NavBar.vue'
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+  <Header/>
+</template>
+<script setup name="Home">
+import Header from '@/components/commonn/Header.vue'
+</script>
